270b45a5ef8a8a69ebf3d6b222d5d5114581f38b
Jessica Joly & Clinton Herrick <student@epicodus.com>
2015-08-06 15:05:49 -0700

    Commit with working Rspec and ruby code

--- coin_combinations/lib/coin_combinations.rb
+++ coin_combinations/lib/coin_combinations.rb
@@ -1,24 +1,46 @@
 class Fixnum
-  self = self.*(100)
-  money = self
-  quarter = 0
-  dime  = 0
-  nickel  = 0
-  penny = 0
+  define_method(:coin_combinations) do
+    money = self
+    quarter = 0
+    dime  = 0
+    nickel  = 0
+    penny = 0
 
-   until money == 0
+    until money == 0 do
       if money.>=(25)
        money = money.-(25)
        quarter = quarter.+(1)
-     elsif money.>=(10)
+       word_array = []
+       word_array.push("You will receive")
+       word_array.push(quarter)
+       word_array.push("quarter")
+       sentence = word_array.join(" ")
+      elsif money.>=(10)
        money = money.-(10)
        dime = dime.+(1)
-     elsif money.>=(5)
+       word_array = []
+       word_array.push("You will receive")
+       word.array.push(dime)
+       word_array.push("dime")
+       sentence = word_array.join(" ")
+      elsif money.>=(5)
        money = money.-(5)
        nickel = nickel.+(1)
-     else money.>=(1)
+       word_array = []
+       word_array.push("You will receive")
+       word.array.push(nickel)
+       word_array.push("nickel")
+       sentence = word_array.join(" ")
+      else money.>=(1)
         money = money.-(1)
-        penny = penny.+(1)         
+        penny = penny.+(1)
+        word_array []
+        word_array.push("You will receive")
+        word_array.push(penny)
+        word_array.push("penny")
+        sentence = word_array.join(" ")
       end
     end
+    return sentence
+  end
 end
--- coin_combinations/spec/coin_combinations_spec.rb
+++ coin_combinations/spec/coin_combinations_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('coin_combinations')
 
 describe('Fixnum#coin_combinations') do
-  it ('receive user money total and return smallest coin total in change') do
-    expect((4.75).coin_combinations()).to(eq("quarter","quarter","quarter"))
-  end
+   it ('receive user cent total and return total amount of coins') do
+    expect((75).coin_combinations()).to(eq("You will receive 3 quarter"))
+   end
 end
