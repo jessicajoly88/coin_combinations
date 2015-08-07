a45c5a00b8f0992d05f0c8d2be41dea17f67f5a8
Jessica Joly & Clinton Herrick <student@epicodus.com>
2015-08-06 17:11:37 -0700

    Commit with completed ruby code and sinatra files

--- coin_combinations/spec/coin_combinations_integration_spec.rb
+++ coin_combinations/spec/coin_combinations_integration_spec.rb
@@ -8,6 +8,6 @@ describe('the coin combinations path', {:type => :feature}) do
     visit('/')
     fill_in('coin', :with => 116)
     click_button('Send')
-    expect(page).to have_content('You will receive 3 quarter')
+    expect(page).to have_content("You will receive 4,quarter,1,dime,1,nickel,1 penny")
   end
 end
--- coin_combinations/spec/coin_combinations_spec.rb
+++ coin_combinations/spec/coin_combinations_spec.rb
@@ -3,6 +3,6 @@ require('coin_combinations')
 
 describe('Fixnum#coin_combinations') do
    it ('receive user cent total and return total amount of coins') do
-    expect((116).coin_combinations()).to(eq("You will receive 3 quarter"))
+    expect((116).coin_combinations()).to(eq("You will receive 4,quarter,1,dime,1,nickel,1 penny"))
    end
 end
